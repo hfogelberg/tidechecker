ddc0829149d8ed604dc7080c247403e2674f8d9a
Henrik Fogelberg <henrik.fogelberg@gmail.com>
2017-03-28 15:24:41 +0100

    Typo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express'),
       app = express(),
       port = process.env.PORT || 80;
 
-app.use('/', express.staitc(__dirname + '/'));
+app.use('/', express.static(__dirname + '/'));
 app.listen(port, ()=>{
   console.log('Tidechecker listening on ' + port);
 });
